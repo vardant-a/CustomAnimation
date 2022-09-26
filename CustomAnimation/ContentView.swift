df3266ca3253c89056247b72a657151f06135273
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-27 00:12:11 +0300

    изменение цвета фона

--- CustomAnimation/ContentView.swift
+++ CustomAnimation/ContentView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Rating()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Rating: View {
+    
+    @State var value: Double = 0.5
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text("Hello, what is your mood?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.indigo)
+                .padding(.top, 20)
+            
+            Spacer(minLength: 0)
+            
+            Slider(value: $value)
+                .padding()
+            
+            Spacer(minLength: 0)
+        }
+        .background(
+            (value <= 0.3 ? Color(.red) : (value > 0.3 && value <= 0.7 ? Color(.yellow) : Color(.green)))
+            )
+        .ignoresSafeArea()
     }
 }
