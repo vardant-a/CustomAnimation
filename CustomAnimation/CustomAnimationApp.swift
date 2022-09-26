ef117b989de29ccb1ef884a8293c44a3b66abd84
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-26 23:53:18 +0300

    Initial Commit

---
+++ CustomAnimation/CustomAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnimationApp.swift
+//  CustomAnimation
+//
+//  Created by Алексей on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
